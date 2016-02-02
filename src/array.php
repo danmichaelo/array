6c42b96f74bfffccd35dab67d0d77fb776697f1a
Andrii Tarykin <m0rt15@Andriis-MacBook-Pro.local>
2016-02-02 11:06:08 +0200

    Initial commit

---
+++ src/array.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Arr;
+
+/**
+ * recursivley expands a flattened array into a heirarchy of arrays
+ * based on a key separator
+ * @param mixed $iterable
+ * @return array returns the same array
+ */
+function expand($iterable, $separator = '.')
+{
+    $array = [];
+    $keys_to_recurse = [];
+     foreach ($iterable as $key => $value) {
+        $sep_position = strpos($key, $separator);
+
+        if ($sep_position === false) {
+            $array[$key] = $value; /* copy over the value verbatim */
+            continue;
+        }
+
+        $prefix = substr($key, 0, $sep_position);
+        $suffix = substr($key, $sep_position + 1);
+
+        if (!array_key_exists($prefix, $array)) {
+            $array[$prefix] = [];
+            $keys_to_recurse[] = $prefix;
+        }
+
+        $array[$prefix][$suffix] = $value;
+     }
+
+     foreach ($keys_to_recurse as $key) {
+        $array[$key] = array_expand($array[$key], $separator);
+     }
+
+     return $array;
+}
+
+/**
+ * Creates a new indexed array same as the old array except the index/key of
+ * the new array elements are the value of array column at $key
+ * @param mixed $iterable the input array
+ * @param string $key the column name to get the index
+ * @return array
+ */
+function index_by($iterable, $key)
+{
+    $map = [];
+    foreach ($iterable as $row) {
+        $map[$row[$key]] = $row;
+    }
+
+    return $map;
+}
+
+/**
+ * Simple function for finding the difference of two arrays based off of a
+ * comparison function. It supposedly does the same as array_udiff, however,
+ * array_udiff does special optimizations create incorrect results if you're comparison
+ * function doesn't return 0, 1, or -1. array_udiff sorts the values to be more efficient
+ * and make less comparisons, but it doesn't work on values that can't really be sorted
+ * @param array $a
+ * @param array $b
+ * @param callable $cmp returns 0 on match, anything else on different
+ */
+function udiff_stable($a, $b, $cmp)
+{
+    $ret = [];
+
+    foreach ($a as $a_val) {
+        $ok = true;
+
+        foreach ($b as $b_val) {
+            if ($cmp($a_val, $b_val) == 0) {
+                $ok = false;
+                break; /* b was in a, so it's excluded in the difference */
+            }
+        }
+
+        if ($ok) {
+            $ret[] = $a_val;
+        }
+    }
+
+    return $ret;
+}
+
+/**
+ * Get an element from the array or return $else
+ * @param array $data
+ * @param string $key
+ * @param mixed $else
+ * @return mixed
+ */
+function get(array $data, $key, $else = null)
+{
+    if (array_key_exists($key, $data)) {
+        return $data[$key];
+    }
+    else {
+        return $else;
+    }
+}
---
+++ tests/ArrayTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Arr\Tests;
+
+use Arr;
+
+class ArrayTest extends \PHPUnit_Framework_TestCase
+{
+    private function arrayExpandGen()
+    {
+        yield 'a' => 1;
+        yield 'b.a' => 2;
+        yield 'b.b' => 3;
+        yield 'c.a.a' => 4;
+        yield 'c.a.b' => 5;
+        yield 'c.b' => 6;
+        yield 'd' => 7;
+    }
+
+    private function arrayIndexColumnGen()
+    {
+        yield [
+            'id' => 'a',
+            'data' => 1,
+        ];
+        yield [
+            'id' => 'c',
+            'data' => 2,
+        ];
+        yield [
+            'id' => 'b',
+            'data' => 3,
+        ];
+        yield [
+            'id' => 'd',
+            'data' => 4,
+        ];
+    }
+
+    public function testArrayExpand()
+    {
+        $expected = [
+            'a' => 1,
+            'b' => [
+                'a' => 2,
+                'b' => 3,
+            ],
+            'c' => [
+                'a' => [
+                    'a' => 4,
+                    'b' => 5,
+                ],
+                'b' => 6,
+            ],
+            'd' => 7,
+        ];
+
+        $this->assertEquals($expected, arr\expand($this->arrayExpandGen()));
+    }
+
+    public function testArrayIndexColumn()
+    {
+        $expected = [
+            'a' => ['id' => 'a', 'data' => 1],
+            'b' => ['id' => 'b', 'data' => 3],
+            'c' => ['id' => 'c', 'data' => 2],
+            'd' => ['id' => 'd', 'data' => 4],
+        ];
+
+        $this->assertEquals(
+            $expected,
+            arr\index_by($this->arrayIndexColumnGen(), 'id')
+        );
+    }
+
+    public function testArrayUdiffStable()
+    {
+        $expected = [1,2];
+
+        $this->assertEquals(
+            $expected,
+            arr\udiff_stable([1,2,3,4], [3,4], function($a, $b){return $a != $b;})
+        );
+    }
+
+    public function testGetElse()
+    {
+        $this->assertTrue(arr\get(['a' => false], 'b', true));
+    }
+    public function testGet()
+    {
+        $this->assertFalse(arr\get(['a' => false], 'a', true));
+    }
+}
